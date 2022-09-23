a7d90583728b3456ac3848b33da3d40e260892c1
abel magicho <32877794+kembAB@users.noreply.github.com>
2022-09-23 19:33:02 +0200

    identity net

---
+++ MVCWebApp/Migrations/20220909122652_AbelEF3.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCWebApp.Migrations
+{
+    public partial class AbelEF3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    CountryName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.CountryName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    LanguageName = table.Column<string>(maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.LanguageName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CityName = table.Column<string>(nullable: false),
+                    CountryForeignKey = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Cities_Countries_CountryForeignKey",
+                        column: x => x.CountryForeignKey,
+                        principalTable: "Countries",
+                        principalColumn: "CountryName",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    CityForeignKey = table.Column<int>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_People_Cities_CityForeignKey",
+                        column: x => x.CityForeignKey,
+                        principalTable: "Cities",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonLanguages",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(nullable: false),
+                    LanguageName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonLanguages", x => new { x.PersonId, x.LanguageName });
+                    table.ForeignKey(
+                        name: "FK_PersonLanguages_Languages_LanguageName",
+                        column: x => x.LanguageName,
+                        principalTable: "Languages",
+                        principalColumn: "LanguageName",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonLanguages_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                column: "CountryName",
+                values: new object[]
+                {
+                    "Sverige",
+                    "Norge",
+                    "Danmark"
+                });
+
+            migrationBuilder.InsertData(
+                table: "Languages",
+                column: "LanguageName",
+                values: new object[]
+                {
+                    "Franska",
+                    "Polska",
+                    "Italienska"
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "ID", "CityName", "CountryForeignKey" },
+                values: new object[] { 1, "Stockholm", "Sverige" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "ID", "CityName", "CountryForeignKey" },
+                values: new object[] { 2, "Oslo", "Norge" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "ID", "CityName", "CountryForeignKey" },
+                values: new object[] { 3, "Köpenhamn", "Danmark" });
+
+            migrationBuilder.InsertData(
+                table: "People",
+                columns: new[] { "ID", "CityForeignKey", "Name", "PhoneNumber" },
+                values: new object[] { 1, 1, "Jens Jensson", "123456789" });
+
+            migrationBuilder.InsertData(
+                table: "People",
+                columns: new[] { "ID", "CityForeignKey", "Name", "PhoneNumber" },
+                values: new object[] { 2, 2, "Anna Andersson", "987654321" });
+
+            migrationBuilder.InsertData(
+                table: "People",
+                columns: new[] { "ID", "CityForeignKey", "Name", "PhoneNumber" },
+                values: new object[] { 3, 3, "Sven Svensson", "123123123" });
+
+            migrationBuilder.InsertData(
+                table: "PersonLanguages",
+                columns: new[] { "PersonId", "LanguageName" },
+                values: new object[] { 1, "Franska" });
+
+            migrationBuilder.InsertData(
+                table: "PersonLanguages",
+                columns: new[] { "PersonId", "LanguageName" },
+                values: new object[] { 2, "Polska" });
+
+            migrationBuilder.InsertData(
+                table: "PersonLanguages",
+                columns: new[] { "PersonId", "LanguageName" },
+                values: new object[] { 3, "Italienska" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CountryForeignKey",
+                table: "Cities",
+                column: "CountryForeignKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_CityForeignKey",
+                table: "People",
+                column: "CityForeignKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonLanguages_LanguageName",
+                table: "PersonLanguages",
+                column: "LanguageName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonLanguages");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
